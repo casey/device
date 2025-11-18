f22b1ccf54ce31ac01b03d9f42a5c976acac6f62
Casey Rodarmor <casey@rodarmor.com>
2025-11-18 15:41:22 -0800

    Amend

--- src/app.rs
+++ src/app.rs
@@ -528,7 +528,7 @@ impl ApplicationHandler for App {
   fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
     if self.renderer.is_none() {
       self.errors.push(Error::internal(format!(
-        "window event recieved before renderer initialization: {event:?}"
+        "window event received before renderer initialization: {event:?}"
       )));
       event_loop.exit();
       return;
