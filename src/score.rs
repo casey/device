fa75545ffbc552f8018479f998605b0b95c5c599
Casey Rodarmor <casey@rodarmor.com>
2025-11-23 13:12:05 -0800

    Synthesizer from voice

--- src/score.rs
+++ src/score.rs
@@ -13,20 +13,19 @@ pub(crate) enum Score {
 impl Score {
   pub(crate) fn synthesizer(self) -> Synthesizer {
     match self {
-      Self::BrownNoise => Synthesizer::new(voice::BrownNoise::new().gain(0.125)),
-      Self::BusySignal => Synthesizer::new(
-        voice::Cycle {
-          inner: voice::Gate {
-            after: 0.5,
-            inner: voice::Sum::new()
-              .add(voice::Sine { frequency: 480.0 })
-              .add(voice::Sine { frequency: 620.0 }),
-          },
-          period: 1.0,
-        }
-        .gain(0.125),
-      ),
-      Self::ClickTrack => Synthesizer::new(voice::Cycle {
+      Self::BrownNoise => voice::BrownNoise::new().gain(0.125).into(),
+      Self::BusySignal => voice::Cycle {
+        inner: voice::Gate {
+          after: 0.5,
+          inner: voice::Sum::new()
+            .add(voice::Sine { frequency: 480.0 })
+            .add(voice::Sine { frequency: 620.0 }),
+        },
+        period: 1.0,
+      }
+      .gain(0.125)
+      .into(),
+      Self::ClickTrack => voice::Cycle {
         period: 2.0 / 3.0,
         inner: voice::Envelope {
           attack: 0.001,
@@ -35,10 +34,11 @@ impl Score {
           release: 0.002,
           inner: voice::BrownNoise::new(),
         },
-      }),
-      Self::PinkNoise => Synthesizer::new(voice::PinkNoise::new().gain(0.125)),
-      Self::Silence => Synthesizer::new(voice::Silence),
-      Self::WhiteNoise => Synthesizer::new(voice::WhiteNoise::new().gain(0.125)),
+      }
+      .into(),
+      Self::PinkNoise => voice::PinkNoise::new().gain(0.125).into(),
+      Self::Silence => voice::Silence.into(),
+      Self::WhiteNoise => voice::WhiteNoise::new().gain(0.125).into(),
     }
   }
 }
