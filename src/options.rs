af6ccfd94a4314069e1e4c37b0c3a631bc3b1041
Casey Rodarmor <casey@rodarmor.com>
2025-11-24 00:33:53 -0800

    Enhance

--- src/options.rs
+++ src/options.rs
@@ -62,14 +62,10 @@ impl Options {
       default()
     };
 
-    if let Some(db) = self.db {
-      state.db = db;
-    }
-
-    if let Some(interpolate) = self.interpolate {
-      state.interpolate = interpolate;
-    }
-
+    state.db = self.db.unwrap_or(state.db);
+    state.fps = self.fps.or(state.fps);
+    state.interpolate = self.interpolate.unwrap_or(state.interpolate);
+    state.resolution = self.resolution.or(state.resolution);
     state.velocity = Vec4f::new(
       self.vx.unwrap_or(state.velocity.x),
       self.vy.unwrap_or(state.velocity.y),
@@ -77,8 +73,6 @@ impl Options {
       self.vw.unwrap_or(state.velocity.w),
     );
 
-    state.fps = self.fps.or(state.fps);
-    state.resolution = self.resolution.or(state.resolution);
     state
   }
 
