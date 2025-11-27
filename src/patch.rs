0f19fdc0685de3741f6c595fb170ba5ba3da9252
Casey Rodarmor <casey@rodarmor.com>
2025-11-26 20:32:37 -0800

    Enhance

--- src/patch.rs
+++ src/patch.rs
@@ -8,12 +8,12 @@ pub(crate) enum Patch {
 }
 
 impl Patch {
-  pub(crate) fn add(self, semitones: u8, mixer: &Tap) {
+  pub(crate) fn add(self, semitones: u8, tap: &Tap) {
     let frequency = 261.63 * 2.0f32.powf(semitones as f32 / 12.0);
 
     match self {
       Self::Saw => {
-        mixer.add(
+        tap.add(
           voice::Saw::new(frequency)
             .envelope(0.001, 0.1, 0.2, 0.1)
             .gain(0.25)
@@ -21,7 +21,7 @@ impl Patch {
         );
       }
       Self::Sine => {
-        mixer.add(
+        tap.add(
           voice::Sine::new(frequency)
             .envelope(0.001, 0.1, 0.2, 0.1)
             .gain(0.25)
