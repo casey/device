c2bb895909646d9a7aec533331bbe13dd0be6c24
Casey Rodarmor <casey@rodarmor.com>
2025-12-15 13:39:40 -0800

    Print spectra bindings

--- src/controller.rs
+++ src/controller.rs
@@ -1,5 +1,13 @@
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd)]
+use super::*;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, IntoStaticStr)]
 pub(crate) enum Controller {
   Spectra,
   Twister,
+}
+
+impl Controller {
+  pub(crate) fn name(self) -> &'static str {
+    self.into()
+  }
 }
