5f5a029bb46d717d70b35fc781dc6c163afc4e6f
Casey Rodarmor <casey@rodarmor.com>
2025-12-21 02:18:19 -0800

    Fix accidental mutual recursion in callback clone

--- src/callback.rs
+++ src/callback.rs
@@ -1,6 +1,8 @@
 use super::*;
 
-pub(crate) trait Callback: FnMut(&mut State, Duration) {
+pub(crate) trait Callback {
+  fn call(&mut self, state: &mut State, dt: Duration);
+
   fn clone_box(&self) -> Box<dyn Callback>;
 }
 
@@ -8,6 +10,10 @@ impl<T> Callback for T
 where
   T: FnMut(&mut State, Duration) + Clone + 'static,
 {
+  fn call(&mut self, state: &mut State, dt: Duration) {
+    self(state, dt);
+  }
+
   fn clone_box(&self) -> Box<dyn Callback> {
     Box::new(self.clone())
   }
