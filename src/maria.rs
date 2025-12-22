9da0d82da35f0cb585face974cf01dd69276ecaf
Casey Rodarmor <casey@rodarmor.com>
2025-12-21 18:20:37 -0800

    Weird cue macro

--- src/maria.rs
+++ src/maria.rs
@@ -1,5 +1,64 @@
 use {super::*, generated::*};
 
+struct Cue {
+  name: &'static str,
+  op: &'static str,
+  beats: Vec<Vec<f32>>,
+}
+
+macro_rules! foo {
+  {
+    $(
+      $bar:literal $op:tt $event:ident $($beat:literal)+ $(/ $($more:literal)+)* ;
+    )*
+  } => {
+    fn foo() -> BTreeMap<u64, Vec<Cue>> {
+      #[allow(unused_mut)]
+      let mut cues = BTreeMap::<u64, Vec<Cue>>::new();
+
+      $(
+        #[allow(unused_mut)]
+        let mut beats = vec![vec![$($beat as f32,)+]];
+
+        $(
+          beats.push(
+            vec![$($more as f32,)+]
+          );
+        )*
+
+        cues.entry($bar).or_default().push(
+          Cue {
+            name: stringify!($event),
+            op: stringify!($op),
+            beats,
+          }
+        );
+      )*
+
+      cues
+    }
+  }
+}
+
+foo! {
+    1 +kick  1 3;
+   39 +kick  2 4;
+   53 +snare 2 4;
+   68 -snare 2 4;
+   71 -kick  2 4;
+   79 +kick  2 4;
+   83 +snare 2 4;
+  123 -snare 2 4;
+  123 -kick  1 2 3 4;
+  123 +kick  1.1 1.2 1.3 2.3 3.3 3.4 4.1 4.2 / 1.1 2.3 3.3 4.2;
+  131 +snare 2 4;
+  147 -snare 2 4;
+  151 -kick  1.1 1.2 1.3 2.3 3.3 3.4 4.1 4.2 / 1.1 2.3 3.3 4.2;
+  151 +kick  1 3;
+  159 -kick  1 3;
+  159 =fade  1;
+}
+
 pub(crate) const SCRIPT: script::Slice = script! (
   3 BLASTER
   15 BLASTER
