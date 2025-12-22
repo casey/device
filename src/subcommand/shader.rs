44749042759082c47154ec8bb7bb3d0ea66ada03
Casey Rodarmor <casey@rodarmor.com>
2025-12-22 01:57:01 -0800

    Validate shaders in shader subcommand

--- src/subcommand/shader.rs
+++ src/subcommand/shader.rs
@@ -1,6 +1,17 @@
 use super::*;
 
 pub(crate) fn run() -> Result {
+  let resolution = 1.try_into().unwrap();
+
+  pollster::block_on(Renderer::new(
+    None,
+    None,
+    resolution,
+    Size::new(resolution, resolution),
+    None,
+  ))?
+  .render(&Analyzer::new(), &State::default(), None)?;
+
   print!("{FilterWgsl}");
   Ok(())
 }
